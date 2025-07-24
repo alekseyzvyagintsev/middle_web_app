750d7daca9b2bb141526791dbb0982a80db82ec3
alekseyzvyagintsev <alex0236889@gmail.com>
2025-07-24 21:34:40 +0300

    созданы логика и шаблоны приложения blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,20 @@
-# from django.urls import path
+from django.urls import path
 from .apps import BlogConfig
-# from .views import ()
+from .views import (BlogEntryCreateView,
+                    IndexListView,
+                    ContactsView,
+                    BlogEntryDetailView,
+                    BlogEntryDeleteView,
+                    BlogEntryUpdateView)
 
 app_name = BlogConfig.name
 
-# urlpatterns = [
-#     path('contact/', ContactView.as_view(), name='contact'),
-#     path('', HomeListView.as_view(), name='home'),
-#     path('home/', HomeListView.as_view(), name='home'),
-#     path('product/new/', ProductCreateView.as_view(), name='create_product'),
+urlpatterns = [
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('index/', IndexListView.as_view(), name='index'),
+    path('blogs/new/', BlogEntryCreateView.as_view(), name='create_blog_entry'),
 #     path('product/list/', ProductListView.as_view(), name='product_list'),
-#     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
-#     path('product/detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-#     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
-# ]
+    path('blogs/delete/<int:pk>/', BlogEntryDeleteView.as_view(), name='entry_delete'),
+    path('blogs/detail/<int:pk>/', BlogEntryDetailView.as_view(), name='entry_detail'),
+    path('blogs/update/<int:pk>/', BlogEntryUpdateView.as_view(), name='entry_update'),
+]
